39783ee2a4baf640993c38e7611fe9c050f4f2d0
Mickael Tremauville <mickael.tremauville@gmail.com>
2026-04-15 20:36:33 +0200

    add more secured password creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,17 @@ class User < ApplicationRecord
          # for Google OmniAuth
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  validate :password_complexity, if: -> { password.present? && provider.blank? }
+
+  private
+
+  def password_complexity
+    return if password.match?(/\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}\z/)
+
+    errors.add :password,
+      "doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial"
+  end
+
   def self.from_omniauth(auth)
     # Find or create a user based on the provider and uid
     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
