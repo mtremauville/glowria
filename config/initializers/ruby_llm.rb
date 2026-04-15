2a29fa203862c798fa096078f06992a9e429432e
Mickael Tremauville <mickael.tremauville@gmail.com>
2026-04-15 20:02:17 +0200

    fixed use_new_acts_as message

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,6 @@
 RubyLLM.configure do |c|
-  c.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
-  c.gemini_api_key    = ENV["GEMINI_API_KEY"]
-  c.openai_api_key = ENV["OPENAI_API_KEY"]
+  c.anthropic_api_key  = ENV["ANTHROPIC_API_KEY"]
+  c.gemini_api_key     = ENV["GEMINI_API_KEY"]
+  c.openai_api_key     = ENV["OPENAI_API_KEY"]
+  c.use_new_acts_as    = true
 end
