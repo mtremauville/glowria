1fd846aeb59f99753f78bd008b4e6fc12a5b12d4
Mickael Tremauville <71016683+mtremauville@users.noreply.github.com>
2026-04-23 23:18:00 +0200

    pwa fixed

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -1,6 +1,8 @@
 class PwaController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
   skip_before_action :check_onboarding, raise: false
+  skip_before_action :verify_authenticity_token, raise: false
+  skip_after_action :verify_same_origin_request!, raise: false
 
   def manifest
     render layout: false
